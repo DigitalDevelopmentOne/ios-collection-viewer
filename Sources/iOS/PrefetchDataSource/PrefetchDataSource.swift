0c5e50f9825ad54933d9ae3b2308579f63c9bd53
cbepxbeo <cbepxbeo@mail.ru>
2023-09-06 04:02:58 +0300

    Create PrefetchDataSource

---
+++ Sources/iOS/PrefetchDataSource/PrefetchDataSource.swift
@@ -0,0 +1,22 @@
+/*
+ 
+ Project: CollectionViewer
+ File: PrefetchDataSource.swift
+ Created by: Egor Boyko
+ Date: 06.09.2023
+ 
+ Status: #In progress | #Not required
+ 
+ */
+
+import SwiftUI
+
+final class PrefetchDataSource<Content: View>: NSObject, UICollectionViewDataSourcePrefetching {
+    weak var coordinator: Coordinator<Content>?
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        prefetchItemsAt indexPaths: [IndexPath]) {
+            self.prefetchItemsAt(collectionView, indexPaths)
+    }
+}

114b5e9e3eee15bd7eb9c79f30529f6d4e7f6a01
cbepxbeo <cbepxbeo@mail.ru>
2023-09-06 04:07:56 +0300

    Create Delegate

---
+++ Sources/iOS/Delegate/Delegate+Method+SizeForItemAt.swift
@@ -0,0 +1,22 @@
+/*
+ 
+ Project: CollectionViewer
+ File: Delegate+Method+SizeForItemAt.swift
+ Created by: Egor Boyko
+ Date: 06.09.2023
+ 
+ Status: #In progress | #Not required
+ 
+ */
+
+import SwiftUI
+
+extension Delegate{
+    @inlinable
+    func sizeForItemAt(
+        _ collectionView: UICollectionView,
+        _ collectionViewLayout: UICollectionViewLayout,
+        _ indexPath: IndexPath) -> CGSize {
+            .zero
+    }
+}
---
+++ Sources/iOS/Delegate/Delegate.swift
@@ -0,0 +1,23 @@
+/*
+ 
+ Project: CollectionViewer
+ File: Delegate.swift
+ Created by: Egor Boyko
+ Date: 06.09.2023
+ 
+ Status: #In progress | #Not required
+ 
+ */
+
+import SwiftUI
+
+final class Delegate<Content: View>: NSObject, UICollectionViewDelegateFlowLayout{
+    weak var coordinator: Coordinator<Content>?
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath) -> CGSize {
+            self.sizeForItemAt(collectionView, collectionViewLayout, indexPath)
+    }
+}

5f00f8ed978231a191e23376464f1c43cc46a9ff
cbepxbeo <cbepxbeo@mail.ru>
2023-09-06 22:09:18 +0300

    Update Delegate+Method+SizeForItemAt.swift

--- Sources/iOS/Delegate/Delegate+Method+SizeForItemAt.swift
+++ Sources/iOS/Delegate/Delegate+Method+SizeForItemAt.swift
@@ -18,14 +18,20 @@ extension Delegate{
         _ collectionViewLayout: UICollectionViewLayout,
         _ indexPath: IndexPath) -> CGSize {
             if let size = self.sizeStorage[indexPath.item] {
+#if DEBUG //----------------------------------------------------------------------------------------
+                self.debugMessage(#function, "Size from storage: \(indexPath.item)")
+#endif //-------------------------------------------------------------------------------------------
                 return size
             }
             guard let content = self.coordinator?.inputData[indexPath.item] else {
                 return .zero
             }
+#if DEBUG //----------------------------------------------------------------------------------------
+            self.debugMessage(#function, "Size calculation: \(indexPath.item)")
+#endif //-------------------------------------------------------------------------------------------
             let hosting = UIHostingController(rootView: content())
             hosting.configure(size: self.coordinator?.ownerSize)
             self.sizeStorage[indexPath.item] = hosting.view.frame.size
             return hosting.view.frame.size
-    }
+        }
 }

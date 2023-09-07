570d8211bf4f9b78f762c3f20fcf1718c6a8a223
cbepxbeo <cbepxbeo@mail.ru>
2023-09-07 03:17:54 +0300

    Update CollectionViewRepresentable+Method+UpdateUIView.swift

--- Sources/iOS/CollectionViewRepresentable/CollectionViewRepresentable+Method+UpdateUIView.swift
+++ Sources/iOS/CollectionViewRepresentable/CollectionViewRepresentable+Method+UpdateUIView.swift
@@ -13,49 +13,10 @@ import SwiftUI
 
 extension CollectionViewRepresentable {
     func updateUIView(_ uiView: UICollectionView, context: Context) {
-        
-        if context.coordinator.inputData.count != self.views.count {
-            
-            if context.coordinator.inputData.count == context.coordinator.dataSource?.data.count {
-                context.coordinator.inputData = self.views
-                context.coordinator.dataSource?.update(uiView)
-            } else {
-                context.coordinator.inputData = self.views
-            }
-        }
-        
-        guard context.coordinator.dataSource == nil  else {
-            return
-        }
-        
-
-        let dataSource: DataSource<Content>
-        if self.views.count > 50 {
-            dataSource = DataSource(data: self.views[0...50].map{$0})
-        } else {
-            dataSource = DataSource(data: self.views)
-        }
-        
-        let prefetchDataSource = PrefetchDataSource<Content>()
-        let delegate = Delegate<Content>()
-        
-        prefetchDataSource.coordinator = context.coordinator
-        dataSource.coordinator = context.coordinator
-        delegate.coordinator = context.coordinator
-        
-        context.coordinator.ownerSize = ownerSize
-
-        context.coordinator.dataSource = dataSource
-        context.coordinator.prefetchDataSource = prefetchDataSource
-        context.coordinator.delegate = delegate
-        
-        uiView.dataSource = dataSource
-        uiView.delegate = delegate
-        uiView.prefetchDataSource = prefetchDataSource
-        uiView.register(Cell<Content>.self, forCellWithReuseIdentifier: "\(Cell<Content>.self)")
-        uiView.decelerationRate = .init(rawValue: 0.9987)
-        uiView.showsVerticalScrollIndicator = false
-        print(uiView.decelerationRate)
- 
+#if DEBUG //----------------------------------------------------------------------------------------
+        self.debugMessage(#function, "Call")
+#endif //-------------------------------------------------------------------------------------------
+        self.configureCoordinator(uiView, context: context)
+        self.updateCoordinator(uiView, context: context)
     }
 }

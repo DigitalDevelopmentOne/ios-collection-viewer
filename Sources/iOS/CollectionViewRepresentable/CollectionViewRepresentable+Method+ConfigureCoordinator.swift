1c06c0ebc57468dbeaa365f01ca6bddc89ec08e3
cbepxbeo <cbepxbeo@mail.ru>
2023-09-07 03:17:46 +0300

    Create CollectionViewRepresentable+Method+ConfigureCoordinator.swift

---
+++ Sources/iOS/CollectionViewRepresentable/CollectionViewRepresentable+Method+ConfigureCoordinator.swift
@@ -0,0 +1,60 @@
+/*
+ 
+ Project: CollectionViewer
+ File: CollectionViewRepresentable+Method+ConfigureCoordinator.swift
+ Created by: Egor Boyko
+ Date: 07.09.2023
+ 
+ Status: #In progress | #Not required
+ 
+ */
+
+import SwiftUI
+
+extension CollectionViewRepresentable {
+    @inlinable
+    func configureCoordinator(_ uiCollection: UICollectionView, context: Context) {
+        if context.coordinator.isConfigured || self.views.isEmpty { return }
+        let coordinator = context.coordinator
+        coordinator.isConfigured = true
+        coordinator.ownerSize = self.ownerSize
+#if DEBUG //----------------------------------------------------------------------------------------
+        self.debugMessage(#function, "Performed")
+#endif //-------------------------------------------------------------------------------------------
+        coordinator.inputData = self.views
+        let dataSource: DataSource<Content>
+        if self.views.count > 50 {
+            dataSource = DataSource(
+                data: self.views[0...50].map{$0},
+                coordinator: coordinator
+            )
+        } else {
+            dataSource = DataSource(
+                data: self.views,
+                coordinator: coordinator
+            )
+        }
+        
+        let prefetchDataSource = PrefetchDataSource<Content>(coordinator: coordinator)
+        let delegate = Delegate<Content>(coordinator: coordinator)
+        
+        coordinator.dataSource = dataSource
+        coordinator.prefetchDataSource = prefetchDataSource
+        coordinator.delegate = delegate
+        
+        coordinator.collectionLayout?.scrollDirection = .vertical
+        coordinator.collectionLayout?.minimumInteritemSpacing = 1
+        coordinator.collectionLayout?.minimumLineSpacing = 1
+        
+        uiCollection.dataSource = dataSource
+        uiCollection.delegate = delegate
+        uiCollection.prefetchDataSource = prefetchDataSource
+        uiCollection.register(
+            Cell<Content>.self,
+            forCellWithReuseIdentifier: "\(Cell<Content>.self)"
+        )
+        uiCollection.decelerationRate = .init(rawValue: 0.9987)
+        uiCollection.showsVerticalScrollIndicator = false
+  
+    }
+}
